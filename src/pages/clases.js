caee281193ed81194b558472a2ad13cd68948821
isabel lopez <ilopezg31@gmail.com>
2021-07-21 01:55:37 +0200

    clases responsive final

--- src/pages/clases.js
+++ src/pages/clases.js
@@ -38,8 +38,8 @@ const ClasesPage = () => {
               </div>
             </Layout>
 
-            <div className="flex-box-half pink-background" id="">
-              <div className="one-half-flex flex-responsive img-absolute" id="relative-side">
+            <div className="flex-box-half pink-background" id="outside-flex">
+              <div className="one-half-flex flex-responsive img-absolute img-circle-class" id="relative-circle">
                   <img src={img13} alt="" class="circle-img-class" />
               </div>
               <div className="one-half-flex" id="outside-text">
@@ -48,23 +48,23 @@ const ClasesPage = () => {
               </div>
           </div>
 
-          <div className="contenido secondary">
-            <div className="flex-box-half margin-40">
-              <div className="one-half-flex">
-                <p className="text center">Otros géneros como <b>Tap, Jazz y Contemporáneo
-                        </b> también siguen un plan de estudio. La colocación del estudiante en una clase se determina por su edad o por la potestad del profesor, mediante la evaluación de su progreso. Cada clase tendrá un conjunto de pasos necesarios que se deberán dominar antes de pasar al siguiente nivel. </p>
-                <p className="text center">Como parte del entrenamiento de nuestras alumnas, una vez llegan a un nivel intermedio se incluyen clases de preparación física como complemento para su formación. Somos fiel creyentes en que debemos entender anatómicamente cómo funciona el cuerpo para lograr con mayor éxito el progreso de las diferentes técnicas de danza que ofrecemos. La conexión mente + cuerpo les dará a nuestras alumnas una visión mas clara de cómo funciona su mayor "instrumento". su cuerpo, y de esa manera aprendan cómo manejarlo correctamente para evitar cualquier tipo de lesiones.</p>
-              </div>
-
-              <div className="one-half-flex img-side" id="relative-side">
-                <div id="background-rounded-class">
+            <div className="contenido" id="secondary-contenido">
+              <div className="flex-box-half margin-40">
+                <div className="one-half-flex">
+                  <p className="text center">Otros géneros como <b>Tap, Jazz y Contemporáneo
+                          </b> también siguen un plan de estudio. La colocación del estudiante en una clase se determina por su edad o por la potestad del profesor, mediante la evaluación de su progreso. Cada clase tendrá un conjunto de pasos necesarios que se deberán dominar antes de pasar al siguiente nivel. </p>
+                  <p className="text center">Como parte del entrenamiento de nuestras alumnas, una vez llegan a un nivel intermedio se incluyen clases de preparación física como complemento para su formación. Somos fiel creyentes en que debemos entender anatómicamente cómo funciona el cuerpo para lograr con mayor éxito el progreso de las diferentes técnicas de danza que ofrecemos. La conexión mente + cuerpo les dará a nuestras alumnas una visión mas clara de cómo funciona su mayor "instrumento". su cuerpo, y de esa manera aprendan cómo manejarlo correctamente para evitar cualquier tipo de lesiones.</p>
                 </div>
-                  <img src={girls} alt="" class="img-class-rounded" />
+
+                <div className="one-half-flex img-side img-flex-class" id="relative-side">
+                  <div id="background-rounded-class">
+                  </div>
+                    <img src={girls} alt="" class="img-class-rounded" id="absolute" />
+                </div>
               </div>
+
+
             </div>
-
-
-          </div>
 
 
         </>
